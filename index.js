ae2092a4e93629ba017f280b390e4739ee0172da
Daeyoung <daeyoungchoi@hotmail.com>
2022-01-18 19:14:12 -0800

    Completed assignmment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+    alert("Yo I was clicked and that wasn't no good!!");
+    }
+
+input.addEventListener('click', clickAlert);
+
+
 }
